ca04d636ef7b30ee79da3f2bbf5ea2b5876e1f25
Alex Ribeiro de Faria <42475620+alexfariakof@users.noreply.github.com>
2024-06-05 17:15:08 -0300

    Scafold branch develop

--- EasyCryptoSalt.UnitTest/coverage_report.ps1
+++ EasyCryptoSalt.UnitTest/coverage_report.ps1
@@ -25,7 +25,6 @@ $coverageXmlPath = Join-Path -Path (Join-Path -Path $projectTestPath -ChildPath 
 
  } 
 
-
 dotnet test ./EasyCryptoSalt.UnitTest.csproj --results-directory $reportPath /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura --collect:"XPlat Code Coverage;Format=opencover" --no-restore --no-build > $null 2>&1
 reportgenerator -reports:$projectTestPath\coverage.cobertura.xml -targetdir:$coverageXmlPath -reporttypes:"Html;lcov;" -sourcedirs:$sourceDirs > $null 2>&1
 Wait-TestResults
