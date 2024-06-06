6e3c9bd72e57f0a0205cb2f54038d68805fb5e25
Alex Ribeiro de Faria <42475620+alexfariakof@users.noreply.github.com>
2024-06-05 22:51:59 -0300

    Refinamento e Otimização do Código

--- EasyCryptoSalt/Crypto.cs
+++ EasyCryptoSalt/Crypto.cs
@@ -72,7 +72,7 @@ public sealed class Crypto : ICrypto
             var config = JsonDocument.Parse(jsonContent);
             config.RootElement.TryGetProperty("CryptoConfigurations", out var cryptoConfigurations);
             cryptoConfigurations.TryGetProperty("Key", out var key);
-            return key.GetString();
+            return key.GetString() ?? throw new();
         }
         catch
         {
@@ -93,7 +93,7 @@ public sealed class Crypto : ICrypto
             var config = JsonDocument.Parse(jsonContent);
             config.RootElement.TryGetProperty("CryptoConfigurations", out var cryptoConfigurations);
             cryptoConfigurations.TryGetProperty("AuthSalt", out var authSalt);
-            return authSalt.GetString();
+            return authSalt.GetString() ?? throw new();
         }
         catch
         {
